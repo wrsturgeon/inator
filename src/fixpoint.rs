665d16a76c2f22b8f44db72746c450eb46a4ccc3
Will Sturgeon <willstrgn@gmail.com>
2023-11-04 16:27:29 -0400

    Allow infix fixpoints (e.g. `a >> fixpoint("f") >> g`)

--- src/fixpoint.rs
+++ src/fixpoint.rs
@@ -11,24 +11,49 @@ use inator_automata::*;
 
 /// Tagged state that can be called later.
 #[must_use = "Fixpoints do nothing unless applied to an automaton with the `>>` operator."]
-#[derive(Clone, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
-pub struct Fixpoint(String);
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Fixpoint<I: Input, S: Stack> {
+    /// Tag that will be associated with the initial state of the right-hand argument to `>>`.
+    tag: String,
+    /// Anything to the left-hand side, e.g. from `... a >> b >> fixpoint("f") >> d`.
+    etc: Option<Deterministic<I, S>>,
+}
 
-impl<I: Input, S: Stack> ops::Shr<Deterministic<I, S>> for Fixpoint {
+impl<I: Input, S: Stack> ops::Shr<Deterministic<I, S>> for Fixpoint<I, S> {
     type Output = Deterministic<I, S>;
     #[inline]
-    #[allow(clippy::manual_assert, clippy::panic)]
+    #[allow(clippy::arithmetic_side_effects, clippy::manual_assert, clippy::panic)]
     fn shr(self, mut rhs: Deterministic<I, S>) -> Self::Output {
-        if rhs.tags.insert(self.0, rhs.initial).is_some() {
+        if let Some(lhs) = self.etc {
+            rhs = lhs >> rhs;
+        }
+        if rhs.tags.insert(self.tag, rhs.initial).is_some() {
             panic!("Fixpoint name already in use");
         }
-        // rhs.sort();
+        rhs
+    }
+}
+
+impl<I: Input, S: Stack> ops::Shr<Fixpoint<I, S>> for Deterministic<I, S> {
+    type Output = Fixpoint<I, S>;
+    #[inline]
+    #[allow(clippy::manual_assert, clippy::panic)]
+    fn shr(self, mut rhs: Fixpoint<I, S>) -> Self::Output {
+        assert!(
+            rhs.etc.is_none(),
+            "Called something of the form `a >> fixpoint(\"f\")`, then \
+            tried to put something else to the left of that same fixpoint object."
+        );
+        rhs.etc = Some(self);
         rhs
     }
 }
 
 /// Name a point in code so we can call it later by name.
 #[inline]
-pub fn fixpoint(call_by_name: &str) -> Fixpoint {
-    Fixpoint(call_by_name.to_owned())
+pub fn fixpoint<I: Input, S: Stack>(call_by_name: &str) -> Fixpoint<I, S> {
+    Fixpoint {
+        tag: call_by_name.to_owned(),
+        etc: None,
+    }
 }
